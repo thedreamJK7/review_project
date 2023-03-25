3132c92658ca86a25e4d69749de0cea036f0a85d
Javohir <jkubaev7@gmail.com>
2023-03-25 15:46:25 +0500

    proyekt saqlandi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import reviews from './data';
+import {FaQuoteRight} from 'react-icons/fa'
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 function App() {
+  const [state, setState] = useState(0)
+  
+  const previousBtn = () => {
+    if(state === reviews.length - 1){
+      setState(0)
+    } else {
+      setState(state + 1);
+    }
+  }
+  const NextBtn = () => {
+    if (state === 0) {
+      setState(reviews.length - 1);
+    } else {
+      setState(state - 1);
+    }
+  };
+  const {id, name, job, image, text} = reviews[state]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h4>{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+          <div className="btn-container">
+            <button onClick={() => previousBtn()} className="prev-btn">
+              <GrFormPrevious />
+            </button>
+            <button onClick={() => NextBtn()} className="next-btn">
+              <GrFormNext />
+            </button>
+          </div>
+          <button className="random-btn" onClick={()=> {
+            let randomNumber = Math.random()*reviews.length
+            if(randomNumber === state) {
+              randomNumber = Math.random() * reviews.length;
+            }
+            setState(Math.round(randomNumber))
+          }}>Surprise me</button>
+        </article>
+      </div>
+    </main>
   );
 }
 
